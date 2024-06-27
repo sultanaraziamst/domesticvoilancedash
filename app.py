e9f63509abd2ca5605f081ba6a9732664501da9f
sultanaraziamst <sultanaraziaofficial@gmail.com>
2024-06-28 02:27:40 +0600

    updategraph

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# app.py
-
 import streamlit as st
 import pandas as pd
 import sqlite3
@@ -18,19 +16,59 @@ def main():
     st.title('Women\'s Violence Data Dashboard')
     st.sidebar.title('Dashboard Menu')
 
-    # Load data
-    df = load_data()
+# Load data
+df = load_data()
 
-    # Display raw data option
-    if st.sidebar.checkbox('Show Raw Data'):
-        st.subheader('Raw Data')
-        st.write(df)
 
-    # Example plot using Plotly
-    st.sidebar.subheader('Data Visualization Options')
-    plot_type = st.sidebar.selectbox('Select a plot type', ['Bar Chart', 'Pie Chart', 'Histogram'])
+#column 
 
-  
+#( SL., No", Age, Education, Employment, Income, "Marital status", Violence)
+
+# Display raw data option
+if st.sidebar.checkbox('Show Raw Data'):
+    st.subheader('Raw Data')
+    st.write(df)
+
+# Example plot using Plotly
+st.sidebar.subheader('Data Visualization Options')
+plot_type = st.sidebar.selectbox('Select a plot type', ['Bar Chart', 'Pie Chart', 'Histogram', 'Scatter Plot'])
+
+
+if plot_type == 'Bar Chart':
+        st.subheader('Bar Chart')
+        fig = px.bar(df, x='Age', y='Education')  # Replace with your columns from your DataFrame
+        st.plotly_chart(fig)
+
+elif plot_type == 'Pie Chart':
+        st.subheader('Pie Chart')
+        fig = px.pie(df, values='Violence', names='Marital status')
+        st.plotly_chart(fig)
+
+elif plot_type == 'Histogram':
+        st.subheader('Histogram')
+        fig = px.histogram(df, x='Income')
+        st.plotly_chart(fig)
+
+elif plot_type == 'Scatter Plot':
+        st.subheader('Scatter Plot')
+        fig = px.scatter(df, x='Age', y='Violence')
+        st.plotly_chart(fig)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
